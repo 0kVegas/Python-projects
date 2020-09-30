653420ef9478fbfa4011500b9406c65a0b122148
Daniel Vega <d.vega@share.epsb.ca>
2020-09-30 09:39:53 -0600

    first file added

---
+++ main.py
@@ -0,0 +1,8 @@
+num = input('give me a number: ')
+
+if int(num) > 5:
+    print('wow this number is bigger than 5')
+elif int(num) == 5:
+    print('wow this number is 5')
+else:
+    print('wow this number is smaller than 5')
