55ccbd96e716dc00e25b007ff77fa5e219904c6f
Daniel Vega <d.vega@share.epsb.ca>
2020-10-08 17:08:57 -0600

    prior to bee movie script test

--- replacement test.py
+++ replacement test.py
@@ -1,9 +1,12 @@
-word = 'hello'
+word = """"""
 guess = ''
 word_length = len(word)
-display = ""
+display = ''
 word_list = ''
 new_word = ''
+found = ''
+wrong_letters = ''
+attempt = 0
 
 
 def replacement(replaced):
@@ -20,19 +23,32 @@ print(replacement(word))
 
 
 def back():
-    global guess, word_list, new_word
-    replacement(word)
+    global guess, word_list, new_word, found, display, wrong_letters, attempt
     while word != new_word:
         guess = input('give me a letter: ')
         if guess in word:
-            found = word.find(guess)
             for i in range(word_length - 1):
+                found = word.find(guess, i)
                 word_list = list(display)
                 word_list[found] = guess
-            new_word = "".join(word_list)
-            print(new_word)
+                if word_list[-1] == word_list[found] and word_list[-1] != word[found]:
+                    word_list[-1] = '_'
+                else:
+                    new_word = "".join(word_list)
+                    display = new_word
+            print(display)
         else:
             print("not found")
+            print(display)
+            wrong_letters = wrong_letters + guess
+            wrong_letters = (wrong_letters + ', ')
+            print('wrong letters: ' + wrong_letters)
+            attempt = attempt + 1
 
 
 print(back())
+print("IT'S DONE")
+if attempt == 1:
+    print('you did it with ' + str(attempt) + ' error')
+else:
+    print('you did it with ' + str(attempt) + ' errors')
